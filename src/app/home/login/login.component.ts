e7083f60a67c56df9c7cb88fd2342cc81fc6b50d
RyamSousa <ryam.sousa123@gmail.com>
2022-05-24 11:46:52 -0300

    :sparkles: added service and autentication function

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { AutenticacaoService } from "src/app/autenticacao/autenticacao.service";
 
 @Component({
 	selector: "app-login",
 	templateUrl: "./login.component.html",
 	styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent implements OnInit {
-	constructor() {}
+export class LoginComponent {
+	usuario: string = "";
+	senha: string = "";
 
-	ngOnInit(): void {}
+	constructor(private authService: AutenticacaoService) {}
+
+	login() {
+		this.authService.autenticar(this.usuario, this.senha).subscribe(
+			() => {
+				console.log("autenticado com sucesso");
+			},
+			(error) => {
+				alert("Usuário ou senha inválido");
+				console.error(error);
+			}
+		);
+	}
 }
