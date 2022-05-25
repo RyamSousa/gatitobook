2299f4147f97dfbcb0a6d8963a74c99d58154224
RyamSousa <ryam.sousa123@gmail.com>
2022-05-25 10:26:16 -0300

    :sparkles: added service and model for create new user

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { NovoUsuario } from "./novo-usuario";
+
+@Injectable({
+	providedIn: "root",
+})
+export class NovoUsuarioService {
+	constructor(private httpClient: HttpClient) {}
+
+	cadastranovoUsuario(novoUsuario: NovoUsuario): Observable<NovoUsuario> {
+		return this.httpClient.post<NovoUsuario>("http://localhost:3000/user/signup", novoUsuario);
+	}
+}
