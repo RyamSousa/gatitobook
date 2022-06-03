c90235998b2d39da8f7c8764a3dad145d0016fbe
RyamSousa <ryam.sousa123@gmail.com>
2022-06-03 09:49:59 -0300

    :sparkles: added template for 'novo-animal' component

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'app-novo-animal',
-  templateUrl: './novo-animal.component.html',
-  styleUrls: ['./novo-animal.component.scss']
+	selector: "app-novo-animal",
+	templateUrl: "./novo-animal.component.html",
+	styleUrls: ["./novo-animal.component.scss"],
 })
-export class NovoAnimalComponent implements OnInit {
+export class NovoAnimalComponent {
+	FormularioAnimal!: FormGroup;
+	file!: File;
+	preview!: string;
+	percentualConcuido: number = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	upload() {}
+	gravaArquivo(arquivo: any) {}
 }
